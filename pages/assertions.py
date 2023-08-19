20f31affe0be511c8d3009cd237393b4e951211a
Liudmila <lsorokina@dejavoosystems.ru>
2023-08-19 17:27:59 +0700

    remove self from assertions and add staticmethod

--- pages/assertions.py
+++ pages/assertions.py
@@ -3,11 +3,12 @@ import json
 
 
 class Assertions:
-    def assert_status_code(self, response:Response, status_code):
+    @staticmethod
+    def assert_status_code(response: Response, status_code):
         assert response.status_code == status_code, f"Wrong status code. Expected: {status_code}. Actual: {response.status_code}"
 
     @staticmethod
-    def assert_json_has_key(self, response:Response, key_name):
+    def assert_json_has_key(response: Response, key_name):
         try:
             response_as_dictionary = response.json()
         except json.JSONDecodeError:
@@ -16,7 +17,7 @@ class Assertions:
         assert key_name in response_as_dictionary, f"Response does not have key = '{key_name}'"
 
     @staticmethod
-    def assert_json_has_value(self, response: Response, value_name, expected_value, error_message):
+    def assert_json_has_value(response: Response, value_name, expected_value, error_message):
         try:
             response_as_dictionary = response.json()
         except json.JSONDecodeError:
